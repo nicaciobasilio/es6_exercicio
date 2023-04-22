6e5a2a3f4464f1b8d7b877b49820a30f299eab29
Nicacio Basilio <nicacioufrn@gmail.com>
2023-04-22 04:56:28 -0300

    finalizando exercicio

---
+++ dist/exercicio.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var alunos = [{
+  nome: 'Maria',
+  nota: 8.5
+}, {
+  nome: 'João',
+  nota: 7.0
+}, {
+  nome: 'Pedro',
+  nota: 9.0
+}, {
+  nome: 'Ana',
+  nota: 6.5
+}, {
+  nome: 'Lucas',
+  nota: 5.5
+}, {
+  nome: 'Bianca',
+  nota: 4.5
+}, {
+  nome: 'Fernanda',
+  nota: 10
+}, {
+  nome: 'Gustavo',
+  nota: 8.0
+}, {
+  nome: 'Henrique',
+  nota: 6.0
+}, {
+  nome: 'Rafaela',
+  nota: 3.5
+}];
+var alunosAcimaDaMedia = alunos.filter(function (notaItem) {
+  return notaItem.nota >= 6;
+});
+console.log(alunosAcimaDaMedia);
